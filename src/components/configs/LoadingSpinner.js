dc46eb388acee1f4a68b258abed67f0193454603
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-16 01:05:13 +0530

    changed spinner color

--- src/components/configs/LoadingSpinner.js
+++ src/components/configs/LoadingSpinner.js
@@ -3,7 +3,7 @@ import React from "react";
 const LoadingSpinner = () => {
     return (
         <div className="flex items-center justify-center h-screen">
-            <div className="border-t-4 border-orange-500 border-solid rounded-full h-16 w-16 animate-spin"></div>
+            <div className="border-t-4 border-blue-500 border-solid rounded-full h-16 w-16 animate-spin"></div>
         </div>
     );
 };
